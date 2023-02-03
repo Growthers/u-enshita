b3276505dd5acee8dba5309e51dbaf5a06a41101
Tatsuto Yamamoto <laminne33569@approvers.dev>
2023-02-03 19:35:56 +0900

    feat: #3 APIリクエスト/レスポンスの型定義

---
+++ pkg/types/api/speaker.go
@@ -0,0 +1,51 @@
+package api
+
+type GetSpeakerResponseJSON struct {
+	Name   string `json:"name"`
+	Title  string `json:"title"`
+	Social []struct {
+		ServiceName string `json:"serviceName"`
+		AccountName string `json:"accountName"`
+	}
+	Duration  uint64 `json:"duration"`
+	BreakTime bool   `json:"breakTime"`
+	Order     uint64 `json:"order"`
+	Cancel    bool   `json:"cancel"`
+}
+
+type UpdateSpeakerRequestJSON struct {
+	Name   string `json:"name"`
+	Title  string `json:"title"`
+	Social []struct {
+		ServiceName string `json:"serviceName"`
+		AccountName string `json:"accountName"`
+	}
+	Duration  uint64 `json:"duration"`
+	BreakTime bool   `json:"breakTime"`
+	Order     uint64 `json:"order"`
+	Cancel    bool   `json:"cancel"`
+}
+
+type UpdateSpeakerResponseJSON struct {
+	Name   string `json:"name"`
+	Title  string `json:"title"`
+	Social []struct {
+		ServiceName string `json:"serviceName"`
+		AccountName string `json:"accountName"`
+	}
+	Duration  uint64 `json:"duration"`
+	BreakTime bool   `json:"breakTime"`
+	Order     uint64 `json:"order"`
+	Cancel    bool   `json:"cancel"`
+}
+
+type ChangeSpeakerWebSocketJSON struct {
+	Name   string `json:"name"`
+	Title  string `json:"title"`
+	Social []struct {
+		ServiceName string `json:"serviceName"`
+		AccountName string `json:"accountName"`
+	}
+	Duration  uint64 `json:"duration"`
+	BreakTime bool   `json:"breakTime"`
+}
