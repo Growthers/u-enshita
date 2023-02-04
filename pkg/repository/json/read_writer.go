3a33a62a49ef26cddd4a79e959f82ee67cf27dd7
Tatsuto Yamamoto <laminne33569@approvers.dev>
2023-02-04 19:53:25 +0900

    feat: テスト用パッケージの追加

--- pkg/repository/json/read_writer.go
+++ pkg/repository/json/read_writer.go
@@ -2,22 +2,24 @@ package json
 
 import (
 	"encoding/json"
-	"io"
-	"os"
-
 	"github.com/growthers/mu-enshita/pkg/types/data"
+	"io"
 )
 
-// JSON Writer
+// DataReadWriter データストアのJSONファイルを操作するための構造体
 type DataReadWriter struct {
-	f *os.File
+	f io.ReadWriteSeeker
 }
 
-func NewDataReadWriter(f *os.File) *DataReadWriter {
+func NewDataReadWriter(f io.ReadWriteSeeker) *DataReadWriter {
 	return &DataReadWriter{f: f}
 }
 
+// write 対象に書き込み
 func (w DataReadWriter) write(d []byte) error {
+	defer w.f.Seek(0, 0)
+
+	// 上書きのために書き込み対象の先頭までシークしておく
 	_, err := w.f.Seek(0, 0)
 	if err != nil {
 		return err
@@ -28,21 +30,15 @@ func (w DataReadWriter) write(d []byte) error {
 		return err
 	}
 
-	_, err = w.f.Seek(0, 0)
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
 
+// read 対象を読み込んでマッピングして返す
 func (w DataReadWriter) read() (data.StoreJSON, error) {
-	d, err := io.ReadAll(w.f)
-	if err != nil {
-		return data.StoreJSON{}, err
-	}
+	defer w.f.Seek(0, 0)
 
-	_, err = w.f.Seek(0, 0)
+	// 対象をすべて読み込み
+	d, err := io.ReadAll(w.f)
 	if err != nil {
 		return data.StoreJSON{}, err
 	}
